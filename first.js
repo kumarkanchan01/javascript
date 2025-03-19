ba67761ea1393fff0196ecec2052374d9a11d00e
kumarkanchan01 <kumarkanchan75990@gmail.com>
2025-03-19 22:28:09 +0530

    started javascript

---
+++ first.js
@@ -0,0 +1,16 @@
+
+console.log(4+5)
+let a = 67
+console.log(a)
+a="harry"
+console.log(a) 
+
+//Arithmetic operator
+/*let a= 15;
+let b = 5;
+console.log("a+b =",a+b)
+console.log("a-b =",a-b)
+console.log("a*b =",a*b)                   
+console.log("a/b =",a/b)
+console.log("a%b =",a%b)
+console.log("a**b =",a**b)*/
